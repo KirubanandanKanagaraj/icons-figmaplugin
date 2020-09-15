4be1e3eef991fdd19bda8799db19413bdcc80b5d
Kirubanandan Kanagaraj <anandh.kbn@gmail.com>
2020-09-15 15:28:25 +0200

    initial-commit1

--- code.js
+++ code.js
@@ -1,1 +1,34 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === 'create-rectangles') {
+        const nodes = [];
+        // for (let i = 0; i < msg.count; i++) {
+        //   const rect = figma.createRectangle();
+        //   rect.x = i * 150;
+        //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+        //   figma.currentPage.appendChild(rect);
+        //   nodes.push(rect);
+        // }
+        const svgObj = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16.2188 16.2188L17.0156 14.9062L12.5156 12.1875V6.98438H11.0156V12.9844L16.2188 16.2188ZM4.92188 4.96875C6.89062 3 9.25 2.01562 12 2.01562C14.75 2.01562 17.0938 3 19.0312 4.96875C21 6.90625 21.9844 9.25 21.9844 12C21.9844 14.75 21 17.1094 19.0312 19.0781C17.0938 21.0156 14.75 21.9844 12 21.9844C9.25 21.9844 6.89062 21.0156 4.92188 19.0781C2.98438 17.1094 2.01562 14.75 2.01562 12C2.01562 9.25 2.98438 6.90625 4.92188 4.96875Z" fill="black"/>
+    </svg>`;
+        const nico = figma.createNodeFromSvg(svgObj);
+        nodes.push(nico);
+        figma.currentPage.selection = nodes;
+        figma.viewport.scrollAndZoomIntoView(nodes);
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+};
