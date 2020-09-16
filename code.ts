5d4d3446ab0709d2224dc1d70cd54e6c42eac01f
Kirubanandan Kanagaraj <anandh.kbn@gmail.com>
2020-09-16 11:36:52 +0200

    svgIcon to figma frame

--- code.ts
+++ code.ts
@@ -14,24 +14,10 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === 'create-icons') {
     const nodes: SceneNode[] = [];
-    // for (let i = 0; i < msg.count; i++) {
-    //   const rect = figma.createRectangle();
-    //   rect.x = i * 150;
-    //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-    //   figma.currentPage.appendChild(rect);
-    //   nodes.push(rect);
-    // }
-
-    const svgObj = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16.2188 16.2188L17.0156 14.9062L12.5156 12.1875V6.98438H11.0156V12.9844L16.2188 16.2188ZM4.92188 4.96875C6.89062 3 9.25 2.01562 12 2.01562C14.75 2.01562 17.0938 3 19.0312 4.96875C21 6.90625 21.9844 9.25 21.9844 12C21.9844 14.75 21 17.1094 19.0312 19.0781C17.0938 21.0156 14.75 21.9844 12 21.9844C9.25 21.9844 6.89062 21.0156 4.92188 19.0781C2.98438 17.1094 2.01562 14.75 2.01562 12C2.01562 9.25 2.98438 6.90625 4.92188 4.96875Z" fill="black"/>
-    </svg>`;
-
-    const nico = figma.createNodeFromSvg(svgObj);    
-    
+    const nico = figma.createNodeFromSvg(msg.svgCode);    
     nodes.push(nico);
-
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
   }
